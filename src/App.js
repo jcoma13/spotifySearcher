373dc4c188a9b059190e7cbc99f5aa2018484771
Jansen Comadena <jansencoma@Jansens-MacBook-Pro-3.local>
2024-09-30 12:57:41 -0700

    adding components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, InputGroup, FormControl, Button, Row, Card } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [ searchInput, setSearchInput ] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+        <InputGroup className='mb-3' size='lg'>
+          <FormControl 
+            placeholder='Search for Artist'
+            type='input'
+            onKeyDown={event => {
+              if (event.key === 'Enter') {
+                console.log('pressed enter')
+              }
+            }}
+            onChange={event => setSearchInput(event.target.value)}
+          />
+          <Button onClick={event => {console.log('Clicked button')}}>
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
     </div>
   );
 }
